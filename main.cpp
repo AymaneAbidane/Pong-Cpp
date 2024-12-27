c9020eff5e8a1c193a6864e72e6866e37d319798
AymaneAbidane <76812961+AymaneAbidane@users.noreply.github.com>
2024-12-27 23:15:17 +0100

    Moving the Ball

--- main.cpp
+++ main.cpp
@@ -8,6 +8,23 @@ const int screenWidth = 1280;
 const int screenHeight = 800;
 const string gameTitle = "The pong Game";
 
+class Ball
+{
+public:
+	float xPos, yPos;
+	int speedX, speedY;
+	int radius;
+	void Draw()
+	{
+		DrawCircle(xPos, yPos, radius, WHITE);
+	}
+	void Move()
+	{
+		xPos += speedX;
+		yPos += speedY;
+	}
+};
+
 const int gameTargetFps = 60;
 
 const float pongBallRadius = 15.0f;
@@ -18,6 +35,9 @@ const int pongPaddlesYPosition = screenHeight / 2 - pongPaddlesHeight / 2;//cent
 const int leftPongPaddleXPosition = 25;
 const int rightPongPaddleXPosition = screenWidth - 25 - leftPongPaddleXPosition;
 
+Ball pongBall;
+
+
 int main(void)
 {
 	cout << "Starting The Game" << endl;//this message will be displayed in terminal
@@ -25,12 +45,24 @@ int main(void)
 	InitWindow(screenWidth, screenHeight, gameTitle.c_str());//initialize the window
 	SetTargetFPS(gameTargetFps);//set the frame rate
 
+	pongBall.radius = pongBallRadius;
+	pongBall.xPos = screenWidth / 2;
+	pongBall.yPos = screenHeight / 2;
+	pongBall.speedX = 7;
+	pongBall.speedY = 7;
+
 	while (WindowShouldClose() == false)//loop until the window is closed and inside of this while loop we will put our game logic
 	{
+
 		BeginDrawing();//begin drawing
 
+		//clear the background In order not to draw another circle on top of the previous one, we need to clear the background before drawing the next frame.
+		ClearBackground(BLACK);
+
+		pongBall.Move();//move the pong ball
+
 		DrawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight, WHITE);//draw the center line
-		DrawCircle(screenWidth / 2, screenHeight / 2, pongBallRadius, WHITE);//draw the pong ball
+		pongBall.Draw();//draw the pong ball
 		DrawRectangle(leftPongPaddleXPosition, pongPaddlesYPosition, pongPaddlesWidth, pongPaddlesHeight, ORANGE);//draw the left paddle
 		DrawRectangle(rightPongPaddleXPosition, pongPaddlesYPosition, pongPaddlesWidth, pongPaddlesHeight, ORANGE);//draw the right paddle
 
